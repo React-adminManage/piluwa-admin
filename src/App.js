1772afa1b48aa541795879cbbabd051b33429fb4
hubing <1349040796@qq.com>
2020-03-28 21:25:14 +0800

    init

--- src/App.js
+++ src/App.js
@@ -9,37 +9,20 @@ import Admin from './pages/Admin'  //引入管理组件
 import ShopList from './pages/Shop/ShopList'
 import ShopAdd from './pages/Shop/ShopAdd'
 
-<<<<<<< HEAD
 // 管理路由
 import admins from './pages/administrator/index'
-=======
 // 订单管理相关组件
 import orderList from './pages/order/orderList'
 import orderAudit from './pages/order/orderAudit'
 import orderDetail from './pages/order/orderDetail'
 
->>>>>>> 77c0ea1a0dac0b7d374c23b7b6cd8799c09a9537
 class App extends Component {
   state = {}
   render() {
     return (
       // 设置路由 
       <HashRouter>
-<<<<<<< HEAD
-        {/* 一级路由模板   路径对应组件 */}
-        <Route path='/login' component={Login}></Route>
-        {/* 嵌套路由模板 */}
-        <Route path='/admin' render={() => {
-          return (
-            <Admin>
-              <Route path='/admin/shop/shopList' component={ShopList}></Route>
 
-              {/* 管理管理路由 */}
-              <Route path='/admin/administrator' component={admins}></Route>
-            </Admin>
-          )
-        }}></Route>
-=======
           {/* 一级路由模板   路径对应组件 */}
           <Route path='/login' component={Login}></Route>
           {/* 嵌套路由模板 */}
@@ -48,6 +31,8 @@ class App extends Component {
               <Admin>
                  <Route path='/admin/shop/shopList' component={ShopList}></Route> 
 
+                {/* 管理管理路由 */}
+                <Route path='/admin/administrator' component={admins}></Route>
 
                 {/* 订单管理的路由 */}
                  <Route exact path='/admin/order/List'   component={orderList}></Route>
@@ -56,7 +41,6 @@ class App extends Component {
               </Admin>
             )
           }}></Route>
->>>>>>> 77c0ea1a0dac0b7d374c23b7b6cd8799c09a9537
       </HashRouter>
     )
   }
