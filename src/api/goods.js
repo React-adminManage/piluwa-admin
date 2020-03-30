b2f6d0d4f66554b35bcf9dada2586efd174e080e
18042567616 <928022781@qq.com>
2020-03-31 01:40:36 +0800

    '4th'

--- src/api/goods.js
+++ src/api/goods.js
@@ -7,8 +7,18 @@ class Goods{
     list(page=1,pageSize=2){
         let url =`${HOST}/shop/find`
         return axios.get(url,{params:{page,pageSize}})
+     
     }
-   
+   del(_id){
+       let url=`${HOST}/shop/del`
+       return axios.post(url,{_id})
+   }
+   putaway(_id,Status){
+       let  url=`${HOST}/shop/changeState`
+       return axios.post(url,{_id,Status})
+       
+   }
 }
 
 export default new Goods()
+  
--- src/pages/Shop/ShopList/index.js
+++ src/pages/Shop/ShopList/index.js
@@ -1,16 +1,16 @@
 import React,{Component, Fragment} from 'react'
 import style from './index.module.less'
-import {Card, message,Table,Tag, Button} from 'antd'
+import {Card, message,Table,Tag, Button,Pagination,Popconfirm,} from 'antd'
 import goodsApi from '../../../api/goods'
 class ShopList extends Component {
-    constructor(){
-        super()
-        this.state = { 
+    
+     
+        state = { 
             page:1, 
             pageSize:4,
             result:[],
-            count:0,
-            columns:[{title: 'ID',dataIndex: 'productId',key: 'productId',width:120,}, 
+            allcount:0,
+            columns:[{title: 'ID',dataIndex: '_id',key: '_id',width:120,}, 
             {title: '名称',dataIndex: 'title',key: 'title',width:120,}, 
             {title: '原价',dataIndex: 'originalPrice',key: 'originalPrice',width:120,render(originalPrice){
                 return(
@@ -39,10 +39,15 @@ class ShopList extends Component {
                 <Tag color={obj[putaway].color}>{obj[putaway].msg}</Tag>
                 )
             }}, 
-            {title: '操作' ,fixed:'right',key: 'action',width:120,render(recode){
+            {title: '操作' ,fixed:'right',key: 'action',width:120,render:(recode)=>{
                 return(
                     <div>
-                        <Button type='danger' size='small'>删除</Button>
+                        <Popconfirm title='你确定要删除这条数据么？' onConfirm={()=>{
+                         this.delGoods(recode._id)
+                        }}>
+                            <Button type='danger' size='small' >删除</Button>
+                        </Popconfirm>
+                       
                         <Button type='warn' size='small'>上架</Button>
                         <Button type='primary' size='small'>修改</Button>
                     </div>
@@ -53,23 +58,30 @@ class ShopList extends Component {
         ]  
           
          }
-    }
+    
    
      componentDidMount(){
          this.getListData()
+     }
+     //删除商品数据
+     delGoods= async (_id)=>{
+         let result = await goodsApi.del(_id)
+         this.getListData() 
      }
      //获取商品数据
     getListData= async ()=>{
         let {page,pageSize} = this.state
         goodsApi.list(page,pageSize).then((pramas)=>{
            this.setState({result:pramas.shopList})
-           console.log(pramas.shopList)
+           this.setState({allcount:pramas.allcount})
+            
         })
 
         
     }
     render() { 
-        let {result, columns} = this.state
+        let {result, columns,allcount,pageSize} = this.state
+        
         return ( 
             <div className={style.box}>    
            
@@ -79,7 +91,22 @@ class ShopList extends Component {
                 <p key={index}>{item._id}</p>
                 )
             })} */}
-            <Table scroll={{y:600,x:500}} columns={columns} dataSource={result}></Table>
+            <Table scroll={{y:600,x:500}} 
+            columns={columns} 
+            pagination={false}
+            dataSource={result}
+            rowKey='_id'>
+
+            </Table>
+            <Pagination showQuickJumper defaultCurrent={1} total={allcount } pageSize={pageSize}  
+            onChange={(page,pageSize)=>{
+                console.log(page)
+                this.setState({page},()=>{
+                    this.getListData()
+                })
+                
+            }}
+            />
             </Card>
             </div>
          );
