eeb6df2250cc1da2b9f6899b82b10fa33f90cd67
hubing <1349040796@qq.com>
2020-04-02 16:25:54 +0800

    home update

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,38 @@
-import React,{Component} from 'react'
+import React,{Component,Fragment} from 'react'
 import Style from './index.module.less'
+import {Card} from 'antd'
 class Home extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className={Style.sayhello}>
-                <img src='ava.png'></img>
-                <span>hello,admin,祝您开心每一天!</span>
-            </div>
+            <Fragment>
+                <div className={Style.sayhello}>
+                    <img src='ava.png'></img>
+                    <span>欢迎登录,祝您开心每一天!</span>
+                </div>
+                <div className={Style.cards}>
+                    
+                    <Card title="商品数量" className={Style.card}>
+                        68
+                    </Card>
+                    <Card title="订单数量" className={Style.card}>
+                    68
+                    </Card>
+                    <Card title="用户数" className={Style.card}>
+                    68
+                    </Card>
+                    <Card title="管理员数" className={Style.card}>
+                    68
+                    </Card>
+                </div>
+                <div className={Style.sale}>
+                    
+                    <Card title="销售量" className={Style.card}>
+                        
+                    </Card>
+                  
+                </div>
+            </Fragment>
          );
     }
 }
